2b49930474c18ccade835945431b2fbe4231c594
Prakriti <subediprakriti11@gmail.com>
2024-12-23 21:31:38 +0545

    enclosing in double quote

--- day3.py
+++ day3.py
@@ -1,5 +1,5 @@
 # this is single line comment
-print("comment is not displayed in output")
+# print("comment is not displayed in output")
 '''i am writing a comment 
 this is multi line comment'''
 '''
@@ -10,3 +10,5 @@ you can also write comment in triple double quote
 this will also work 
 here is the demo
 """
+print("This is the demo for \"enclosing inside double quote\" " )
+print("Use \"something\" for enclosing inside double quote")
