023ee2a3f8cc1f97fcbc5e3c03bb017d5c042833
Prakriti <subediprakriti11@gmail.com>
2024-12-23 21:02:30 +0545

    contd..

--- day3.py
+++ day3.py
@@ -5,3 +5,8 @@ this is multi line comment'''
 '''
 to write multiline comments, we can write comment inside triple quote
 '''
+"""
+you can also write comment in triple double quote
+this will also work 
+here is the demo
+"""
