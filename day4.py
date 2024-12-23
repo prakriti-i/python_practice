e779d86873a33d8c3292489836da61501ea9e3e9
Prakriti <subediprakriti11@gmail.com>
2024-12-23 22:38:00 +0545

    additional datatypes

--- day4.py
+++ day4.py
@@ -1,5 +1,5 @@
 a=1
-a1=3
+a1=3.1
 b=True
 c=None
 d="hi"
@@ -7,6 +7,17 @@ print("type of a is", type(a))
 print("type of b is", type(b))
 print("type of c is", type(c))
 print("type of d is", type(d))
+print("type of a1 is", type(a1))
 print(a+a1)
+i=complex(1, 4)
+print("type of i is", type(i))
+list=[1,2,3, [4,-7], "Ram"]
+tuple=(("parrot","sparrow"),("lion","tiger")) 
+# cannot change the value of tuple
+dict={"name":"Ram", "age":19}
+print(list)
+print(dict)
+print(tuple)
+
 
 
