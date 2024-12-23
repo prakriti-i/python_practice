67bbabdefba2253c2405c2b57d0bb2b881f1f015
Prakriti <subediprakriti11@gmail.com>
2024-12-23 20:57:46 +0545

    multi line comment

---
+++ day3.py
@@ -0,0 +1,7 @@
+# this is single line comment
+print("comment is not displayed in output")
+'''i am writing a comment 
+this is multi line comment'''
+'''
+to write multiline comments, we can write comment inside triple quote
+'''
